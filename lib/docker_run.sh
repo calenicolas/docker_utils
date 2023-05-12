1005ff2230893c1b0ac2c1138b7542c211bafc39
claudio.ale <claudio.ale@etermax.com>
2023-05-12 17:49:43 -0300

    fixes

--- lib/docker_run.sh
+++ lib/docker_run.sh
@@ -6,15 +6,23 @@ function docker_run() {
   local NETWORK="$3"
   local IMAGE_NAME="$4"
 
-  if ! docker network ls | grep -w "$NETWORK"
+  local NETWORK_EXISTS
+  NETWORK_EXISTS=$(docker network ls | grep -w "$NETWORK")
+
+  if ! "$NETWORK_EXISTS"
   then
     docker network create --opt com.docker.network.bridge.name="$NETWORK" "$NETWORK" > /dev/null 2>&1 &
   fi
 
+  local CONTAINER_ID
   CONTAINER_ID=$(docker run -it --rm \
     --name "$CONTAINER_NAME" \
     -p "$SERVICE_PORT":"$SERVICE_PORT" \
     --network "$NETWORK" \
     -d "$IMAGE_NAME")
-  docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$CONTAINER_ID"
+
+  local CONTAINER_IP
+  CONTAINER_IP=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "$CONTAINER_ID")
+
+  echo "$CONTAINER_IP"
 }
